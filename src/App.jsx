ca26794dcc9446678518fe870a2ff74827a72d2a
Jose Albornoz <prueba@outlook.cl>
2023-09-08 23:08:35 -0300

    la etiqueta main que estaba en el componente App, la muevo al componente MiApi

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,7 @@ function App() {
   return (
     <>
       <Header />
-      <main>
-        <MiApi />
-      </main>
+      <MiApi />
       <Footer />
     </>
   );
--- src/componentes/MiApi/MiApi.jsx
+++ src/componentes/MiApi/MiApi.jsx
@@ -91,11 +91,11 @@ export default function MiApi() {
     ));
 
   return (
-    <>
+    <main>
       <Buscador searchGames={setSearch} sortGames={setSort} />
       <div className="games row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 g-3">
         {showGames}
       </div>
-    </>
+    </main>
   );
 }
